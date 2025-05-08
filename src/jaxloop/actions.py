e1570250d77081e4d2cc794208cccfe48a0f2979
Jaxloop Team <jaxloop-team@google.com>
2025-05-08 17:37:06 +0000

    No public description
    PiperOrigin-RevId: 756371337

--- src/jaxloop/actions.py
+++ src/jaxloop/actions.py
@@ -14,8 +14,10 @@
 
 """The action library for JAX models."""
 
+import collections
 import typing
 from typing import Any, Callable, Optional, Protocol
+from collections.abc import Mapping
 
 from absl import logging
 from clu import metric_writers
@@ -31,6 +33,9 @@ State = types.TrainState
 Scalar = types.Scalar
 PyTree = jaxtyping.PyTree
 ApplyFn = Callable[[PyTree, PyTree], PyTree]
+
+MetricWithMetadata = types.MetricWithMetadata
+MetricType = types.MetricType
 
 
 @typing.runtime_checkable
@@ -51,7 +56,7 @@ class Action(Protocol):
     """
     super().__init__()
     if interval <= 0:
-      raise ValueError('`interval` must be positive.')
+      raise ValueError("`interval` must be positive.")
     self._interval = interval
 
   def __call__(
@@ -71,7 +76,7 @@ class Action(Protocol):
     Returns:
       Optional return value or None.
     """
-    raise NotImplementedError('Action\'s `__call__` method is not implemented.')
+    raise NotImplementedError("Action's `__call__` method is not implemented.")
 
   @property
   def interval(self) -> int:
@@ -79,7 +84,19 @@ class Action(Protocol):
 
 
 class SummaryAction(Action):
-  """Summary action."""
+  """Summary action.
+
+  Only the types included in the supported_types set are supported by the
+  summary action.
+  """
+
+  supported_types = {
+      MetricType.SCALAR,
+      MetricType.HISTOGRAM,
+      MetricType.IMAGE,
+      MetricType.VIDEO,
+      MetricType.TEXT,
+  }
 
   def __init__(
       self,
@@ -95,9 +112,56 @@ class SummaryAction(Action):
   def summary_writer(self) -> metric_writers.MetricWriter:
     return self._summary_writer
 
+  def split_outputs(
+      self, outputs: Output | None
+  ) -> Mapping[MetricType, dict[str, Any]]:
+    """Splits the outputs by type for dispatching to different writing methods.
+
+    Args:
+      outputs: The model output.
+
+    Returns:
+      A dictionary of outputs, split by metric type.
+    """
+    if outputs is None:
+      return {}
+
+    outputs_by_type = collections.defaultdict(dict)
+    for key, value in outputs.items():
+      # This means it is legacy usage with bare type.
+      if not isinstance(value, MetricWithMetadata):
+        outputs_by_type[MetricType.SCALAR][key] = value
+      else:
+        # Not all types are supported at the moment.
+        if value.type not in self.supported_types:
+          raise ValueError(
+              f"Metric type {value.type} is not supported by the summary"
+              f" writer, supported types are: {self.supported_types}"
+          )
+        outputs_by_type[value.type][key] = value.value
+    return outputs_by_type
+
   def __call__(self, state: State, outputs: Optional[Output], **kwargs) -> None:
+    """Writes the data to the summary writer."""
+
     step = int(state.step)
-    self._summary_writer.write_scalars(step, outputs)
+    # We write the data according to the type, using a different writing
+    # method for each of the supported types.
+    for metric_type, data in self.split_outputs(outputs).items():
+      if metric_type == MetricType.SCALAR:
+        self._summary_writer.write_scalars(step, data)
+      elif metric_type == MetricType.HISTOGRAM:
+        self._summary_writer.write_histograms(step, data)
+      elif metric_type == MetricType.IMAGE:
+        self._summary_writer.write_images(step, data)
+      elif metric_type == MetricType.VIDEO:
+        self._summary_writer.write_videos(step, data)
+      elif metric_type == MetricType.TEXT:
+        self._summary_writer.write_texts(step, data)
+      else:
+        raise ValueError(
+            f"Split outputs contains unsupported metric type: {metric_type}"
+        )
     if self._flush_each_call:
       self._summary_writer.flush()
 
@@ -136,13 +200,13 @@ class CheckpointAction(Action):
     )
     if succeeded:
       logging.info(
-          'Saved checkpoint to %s on step %d.',
+          "Saved checkpoint to %s on step %d.",
           self._ckpt_manager.directory,
           step,
       )
     else:
       logging.error(
-          'Saving checkpoint to %s on step %d failed.',
+          "Saving checkpoint to %s on step %d failed.",
           self._ckpt_manager.directory,
           step,
       )
--- src/jaxloop/tests/actions_test.py
+++ src/jaxloop/tests/actions_test.py
@@ -14,13 +14,19 @@
 
 """Unit tests for actions."""
 
-# (TODO: b/347771130): Add unit tests for other actions.
+
+import dataclasses
+from typing import cast
+from unittest import mock
 
 from absl.testing import absltest
+import chex
+import jax.numpy as jnp
 from jaxloop import actions
 from jaxloop import types
 from jaxloop.step_number_writer import step_number_writer
 import optax
+
 
 State = types.TrainState
 
@@ -47,7 +53,7 @@ class ActionsTest(absltest.TestCase):
   def test_step_number_action(self):
     state = State.create(
         apply_fn=None,
-        params={'params': 1},
+        params={"params": 1},
         tx=optax.identity(),
     ).replace(step=1)
     step_writer = FakeStepNumberWriter()
@@ -56,5 +62,82 @@ class ActionsTest(absltest.TestCase):
     self.assertEqual(step_writer.step_numbers(), [1])
 
 
-if __name__ == '__main__':
+class SummaryActionTest(absltest.TestCase):
+
+  def test_summary_action(self):
+    @dataclasses.dataclass
+    class MockedState:
+      step: int
+
+    histograms = []
+    scalars = []
+
+    mocked_writer = mock.MagicMock()
+    mocked_writer.write_histograms = lambda step, data: histograms.append(
+        (step, data)
+    )
+    mocked_writer.write_scalars = lambda step, data: scalars.append(
+        (step, data)
+    )
+    action = actions.SummaryAction(
+        summary_writer=mocked_writer,
+        interval=1,
+        flush_each_call=True,
+    )
+
+    outputs = {
+        "a": actions.MetricWithMetadata(
+            jnp.array([1.0, 2.0]), type=types.MetricType.HISTOGRAM
+        ),
+        "b": actions.MetricWithMetadata(
+            jnp.array([3.0, 4.0]), type=types.MetricType.HISTOGRAM
+        ),
+        "c": jnp.array([5.0, 6.0]),
+        "d": actions.MetricWithMetadata(1.5, type=types.MetricType.SCALAR),
+    }
+    action(cast(types.TrainState, MockedState(1)), outputs)
+
+    chex.assert_trees_all_equal(
+        histograms,
+        [
+            (1, {"a": jnp.array([1.0, 2.0]), "b": jnp.array([3.0, 4.0])}),
+        ],
+    )
+    chex.assert_trees_all_equal(
+        scalars, [(1, {"c": jnp.array([5.0, 6.0]), "d": 1.5})]
+    )
+
+  def test_unsupported_type(self):
+    @dataclasses.dataclass
+    class MockedState:
+      step: int
+
+    histograms = []
+    scalars = []
+
+    mocked_writer = mock.MagicMock()
+    mocked_writer.write_histograms = lambda step, data: histograms.append(
+        (step, data)
+    )
+    mocked_writer.write_scalars = lambda step, data: scalars.append(
+        (step, data)
+    )
+    action = actions.SummaryAction(
+        summary_writer=mocked_writer,
+        interval=1,
+        flush_each_call=True,
+    )
+
+    outputs = {
+        # Adding unsupported type here. This should raise an error.
+        "a": actions.MetricWithMetadata(
+            jnp.array([1.0, 2.0]), type=types.MetricType.AUDIO
+        ),
+    }
+    with self.assertRaises(ValueError):
+      action(cast(types.TrainState, MockedState(1)), outputs)
+
+
+
+if __name__ == "__main__":
   absltest.main()
